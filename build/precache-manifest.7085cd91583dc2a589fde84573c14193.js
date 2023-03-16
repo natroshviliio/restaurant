b3a420a8a1c6a3a6f27de32fdeb0a08d3c9008d2
Ioane <ioane.natroshvili08b@geolab.edu.ge>
2023-03-16 18:46:18 +0400

    rest

---
+++ build/precache-manifest.7085cd91583dc2a589fde84573c14193.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1bcc810367a69586866",
+    "url": "/static/css/main.2cdc247e.chunk.css"
+  },
+  {
+    "revision": "a1bcc810367a69586866",
+    "url": "/static/js/main.a1bcc810.chunk.js"
+  },
+  {
+    "revision": "58bd2bf2d128b8229b87",
+    "url": "/static/css/1.9e5e6f48.chunk.css"
+  },
+  {
+    "revision": "58bd2bf2d128b8229b87",
+    "url": "/static/js/1.58bd2bf2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "494426086178b9a17069cfee70838cbc",
+    "url": "/static/media/section6-1.49442608.jpg"
+  },
+  {
+    "revision": "f8c6701efb1b35a0950ed4c2f266e28d",
+    "url": "/static/media/slide6.f8c6701e.jpg"
+  },
+  {
+    "revision": "fc808afc138710dc2e15341d13e0fad9",
+    "url": "/static/media/burger2.fc808afc.png"
+  },
+  {
+    "revision": "3018834bf309c5515be6f4c643fcdd22",
+    "url": "/static/media/wave.3018834b.svg"
+  },
+  {
+    "revision": "f00f8a87407d70e40b1d9ec24cc4ddbc",
+    "url": "/static/media/picture3.f00f8a87.png"
+  },
+  {
+    "revision": "44a0be57dc6a373a1782e37fcd753cb4",
+    "url": "/static/media/picture5.44a0be57.png"
+  },
+  {
+    "revision": "25454c63dfe79cc133ee5e719e8848ab",
+    "url": "/static/media/picture6.25454c63.jpg"
+  },
+  {
+    "revision": "ed9dd6c12b0f6801056da86db8e32341",
+    "url": "/static/media/picture7.ed9dd6c1.png"
+  },
+  {
+    "revision": "2afc6fe0933f04d9e06e66501e8f1fe7",
+    "url": "/static/media/picture8.2afc6fe0.png"
+  },
+  {
+    "revision": "5fc8c466aa85961f0a846b8673199f9a",
+    "url": "/static/media/xinkali1.5fc8c466.jpg"
+  },
+  {
+    "revision": "aabce547da72278319cea165bf57d2a7",
+    "url": "/static/media/xink1.aabce547.jpg"
+  },
+  {
+    "revision": "02d4bb46136624c7699fae8d5cc1068c",
+    "url": "/static/media/xink2.02d4bb46.jpg"
+  },
+  {
+    "revision": "5f8ae222f47881965c8867c6a7deeb5c",
+    "url": "/static/media/xink3.5f8ae222.jpg"
+  },
+  {
+    "revision": "c9c4c8c1cfdb95063d72e2c0e7949e69",
+    "url": "/static/media/xink4.c9c4c8c1.jpg"
+  },
+  {
+    "revision": "c146e9c0b002b15c65e185e39f400172",
+    "url": "/static/media/xink5.c146e9c0.jpg"
+  },
+  {
+    "revision": "465b7e8436e1696a365d1cfc2f538975",
+    "url": "/static/media/xink6.465b7e84.jpeg"
+  },
+  {
+    "revision": "ac5bf56126b6d3de50b3ad711eb32e85",
+    "url": "/static/media/xachapuri2.ac5bf561.jpg"
+  },
+  {
+    "revision": "30bdd6a36e4637ce762acfc17fa31b5a",
+    "url": "/static/media/xachapuri3.30bdd6a3.jpg"
+  },
+  {
+    "revision": "cda2511a6fe51e843663bf3dcc12e0fc",
+    "url": "/static/media/xachapuri4.cda2511a.jpg"
+  },
+  {
+    "revision": "9001a1df0ce5f9471bca6ae8b269de25",
+    "url": "/static/media/xachapuriLogo.9001a1df.jpg"
+  },
+  {
+    "revision": "7c8170f7cb5d617c4dae8400ac2ff921",
+    "url": "/static/media/cocktailBackground.7c8170f7.jpg"
+  },
+  {
+    "revision": "9be0da781571386ee76ad14688fc234b",
+    "url": "/static/media/restaurant.9be0da78.jpg"
+  },
+  {
+    "revision": "bd98d3bca6f40e833f32e6b651f1af2d",
+    "url": "/static/media/cocktail1.bd98d3bc.jpg"
+  },
+  {
+    "revision": "0681e8742c91c00c572815a06a595594",
+    "url": "/static/media/cocktail2.0681e874.jpg"
+  },
+  {
+    "revision": "26baeaf55fb30bde83d6db7b185f7598",
+    "url": "/static/media/cocktail3.26baeaf5.jpg"
+  },
+  {
+    "revision": "a1b803fed6becc5ce7bd87fd4b29f92e",
+    "url": "/static/media/cocktail4.a1b803fe.jpg"
+  },
+  {
+    "revision": "2acceb1fa0ae0dcac761d66699aa88f2",
+    "url": "/static/media/cocktail5.2acceb1f.jpg"
+  },
+  {
+    "revision": "59e0489bf813e6ac6daae382adb28b32",
+    "url": "/static/media/cocktail6.59e0489b.jpg"
+  },
+  {
+    "revision": "99bcfaccb993aca24139719d1d7cf53b",
+    "url": "/static/media/smoothie1.99bcfacc.jpg"
+  },
+  {
+    "revision": "4cd333ee0db5a6858843c8b22ebda326",
+    "url": "/static/media/smoothie2.4cd333ee.png"
+  },
+  {
+    "revision": "36d67b3ac6976abf16f723e4f5d56a61",
+    "url": "/static/media/smoothie3.36d67b3a.jpg"
+  },
+  {
+    "revision": "c3cb0ca271681889bd8633617958909a",
+    "url": "/static/media/smoothie4.c3cb0ca2.jfif"
+  },
+  {
+    "revision": "124aa8970be7b1b71e94771b97f6397c",
+    "url": "/static/media/smoothie6.124aa897.jpg"
+  },
+  {
+    "revision": "544fe3e665e38b547f00735156535c7d",
+    "url": "/static/media/dessert3.544fe3e6.jpg"
+  },
+  {
+    "revision": "485e34f5a6d315e46e13c6e7a27ae418",
+    "url": "/static/media/dessert4.485e34f5.jpg"
+  },
+  {
+    "revision": "e3956c60e34499799c914fcca0b968f9",
+    "url": "/static/media/dessert8.e3956c60.jpg"
+  },
+  {
+    "revision": "b0a5973c7109b89dcc697778fe249313",
+    "url": "/static/media/dessert9.b0a5973c.jpg"
+  },
+  {
+    "revision": "d2bcc2cdec8b9b84d0230dd13da95979",
+    "url": "/static/media/dessert6.d2bcc2cd.jpg"
+  },
+  {
+    "revision": "46bcaa7293dee5d02feb2e22eedb92da",
+    "url": "/static/media/dessert10.46bcaa72.jpg"
+  },
+  {
+    "revision": "6700eb795c6f0653eff698b76a0b6f1d",
+    "url": "/static/media/waffle.6700eb79.jpg"
+  },
+  {
+    "revision": "310f5ecabace014dd60dd199914a1e3f",
+    "url": "/static/media/pancake1.310f5eca.jpg"
+  },
+  {
+    "revision": "abe3232a163fa9d21c95d9d93a7bee9f",
+    "url": "/static/media/dessert2.abe3232a.jpg"
+  },
+  {
+    "revision": "6dfba45e4da188eaa86ea9c62ebf2451",
+    "url": "/static/media/slide1.6dfba45e.jpg"
+  },
+  {
+    "revision": "0aa4f73657fd1a7533a2c765af4f887e",
+    "url": "/static/media/slide2.0aa4f736.jpg"
+  },
+  {
+    "revision": "63063e915700e1ee87c20d208920f417",
+    "url": "/static/media/slide3.63063e91.jpg"
+  },
+  {
+    "revision": "61985886c09802201666df8fee7f10d1",
+    "url": "/static/media/slide4.61985886.jfif"
+  },
+  {
+    "revision": "cc30df829b1fd01c5fb39651857b96fe",
+    "url": "/static/media/slide5.cc30df82.jpg"
+  },
+  {
+    "revision": "3f975146a78d7ff8d1f20297349db24e",
+    "url": "/static/media/lastCake1.3f975146.webp"
+  },
+  {
+    "revision": "5c6fd45bd0f193936df67dc7f9772d0b",
+    "url": "/static/media/lastCake2.5c6fd45b.jpg"
+  },
+  {
+    "revision": "4d1b3011a8320b4b5c63a96fe788aead",
+    "url": "/static/media/smoothie5.4d1b3011.jpg"
+  },
+  {
+    "revision": "02449fe3fc97a4710cdd686ccb382c06",
+    "url": "/static/media/dessert5.02449fe3.jpg"
+  },
+  {
+    "revision": "538e8e3e925bd975ccff1e0e736dacc9",
+    "url": "/static/media/dessert7.538e8e3e.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "1f2651021f80b1f211bc44660abb828e",
+    "url": "/static/media/slide8.1f265102.jfif"
+  },
+  {
+    "revision": "73f0c3652f552167c2fafcb7cce57cd9",
+    "url": "/static/media/section6-2.73f0c365.jpg"
+  },
+  {
+    "revision": "78932c17e9096a5b31ecd94eac9fa72a",
+    "url": "/static/media/section6-3.78932c17.jpg"
+  },
+  {
+    "revision": "64362136ac9b63adcb645418e4ad2ceb",
+    "url": "/index.html"
+  }
+];
